c1afce6edba6981b59a9d92dcf4d4d7fe57d6290
Luciano Fosco <lucianofosco@gmail.com>
2024-03-13 21:08:38 -0300

    Mi primer commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ComponenteNuevo from '../ComponenteNuevo'
+function App() {
+  
+console.log("Hola desde react")
+  return (
+    <>
+  <h1>Hola mundo desde React</h1>
+  <p>te queremos</p>
+  <ComponenteNuevo></ComponenteNuevo>
+  </>
+  )
+}
+
+export default App
